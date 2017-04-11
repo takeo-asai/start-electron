884b3b288561813e0163dd1d2c7cf50291bf319c
takeo asai <t_asai@interspace.ne.jp>
2017-04-11 16:38:34 +0900

    Add router and material-ui

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import { render } from "react-dom";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+import Sidebar from './sidebar';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
 
 render(
-    <div>
-        Hello, Eletron and React JSX
-    </div>,
+    <Router>
+        <MuiThemeProvider>
+            <div>
+                <Link to="/about">About</Link>
+                <Link to="/">Home</Link>
+                <Sidebar />
+                <FloatingActionButton style={{ marginRight: 20, }}>
+                    <ContentAdd />
+                </FloatingActionButton>
+
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+            </div>
+        </MuiThemeProvider>
+    </Router>,
     document.getElementById("app")
 );

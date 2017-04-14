e07f9a50e8d434eb45866fc49043d6b26f7d18aa
takeo asai <t_asai@interspace.ne.jp>
2017-04-14 10:16:53 +0900

    Add transparent window config

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ import { app, BrowserWindow } from "electron";
 
 let win;
 function createWindow() {
-    win = new BrowserWindow({ width: 800, height: 600 });
+    win = new BrowserWindow({ width: 800, height: 600,
+//        transparent: true,
+//        frame: false,
+});
     win.loadURL(`file://${__dirname}/index.html`);
     win.on("closed", () => {
         win = null;

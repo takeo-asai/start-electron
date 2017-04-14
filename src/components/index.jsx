4152ddf85b40a87ef2f228de20eb8221bfd596ed
takeo asai <t_asai@interspace.ne.jp>
2017-04-14 12:05:50 +0900

    Format style

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import FloatingActionButton from 'material-ui/FloatingActionButton';
-import ContentAdd from 'material-ui/svg-icons/content/add';
-
-import injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
-import Sidebar from './sidebar';
-import Stepper from './stepper';
-import About from './about';
-
 import {
     BrowserRouter as Router,
     Route,
     Link
 } from 'react-router-dom'
 
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import Sidebar from './sidebar';
+import Stepper from './stepper';
+import About from './about';
+import Home from './home';
 import styles from './style.css';
 
 render(
